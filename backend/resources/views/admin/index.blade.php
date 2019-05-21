cb6446b623efc10d90cd4a6cb8a0bc7d5e3bd551
Efrat19 <efrat890089@gmail.com>
2019-05-21 09:25:50 +0300

    spatie/laravel-server-side-rendering

--- backend/resources/views/admin/index.blade.php
+++ backend/resources/views/admin/index.blade.php
@@ -1,16 +1,17 @@
-<script src="{{ asset('js/admin.js') }}" defer></script>
-<div id="app">
+{{--<script src="{{ asset('js/admin.js') }}" defer></script>--}}
+{!! ssr('js/admin.js')->render() !!}
+<div id="app">>>
     <h1>admin page</h1>
-    <details>
-        <summary>Users</summary>
-        <search></search>
-    </details>
-    <details>
-        <summary>Homeworks</summary>
-    </details>
-    <details>
-        <summary>Comments</summary>
+    <search url="/admin/search/users" result-type="user"></search>
+    {{--<details>--}}
+        {{--<summary>Users</summary>--}}
+    {{--</details>--}}
+    {{--<details>--}}
+        {{--<summary>Homeworks</summary>--}}
+    {{--</details>--}}
+    {{--<details>--}}
+        {{--<summary>Comments</summary>--}}
 
-    </details>
+    {{--</details>--}}
     <button onclick="window.foo = 'bar'">clickme</button>
 </div>
